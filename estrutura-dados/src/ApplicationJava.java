f15760e9c97ccc7e7df916c260087b2319275502
Danilo Almeida <48030619+DaniloAlmeidaSantos@users.noreply.github.com>
2022-09-09 20:34:42 -0300

    Add files via upload

---
+++ estrutura-dados/src/ApplicationJava.java
@@ -0,0 +1,10 @@
+
+
+public class ApplicationJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ estrutura-dados/src/main/java/ApplicationJava.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class ApplicationJava {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
